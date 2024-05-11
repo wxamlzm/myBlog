c80ac353f730de17fcd12d4eca35f0913a1a78a2
wxamlzm <zd5024@163.com>
2024-05-11 20:01:17 +0800

    UPDATE：test route

--- python/helloWorld.py
+++ python/helloWorld.py
@@ -4,7 +4,31 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "hello world"
+def index():
+    return "index page"
+
+
+@app.route("/hello")
+def hello():
+    return "Hello, World"
+
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    # show thie user profile for that user
+    return f"User {escape(username)}"
+
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    # show thie post with the given id, the id is an integer
+    return f"Post {post_id}"
+
+
+@app.route("/path/<path:subpath>")
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f"Subpath {escape(subpath)}"
+
 
 app.run()
